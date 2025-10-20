7619f8debeb8ae313b2e02c13b442e4b6d4c9055
xshaikhah <shai5a.alharthi@gmail.com>
2025-10-20 05:53:02 +0500

    nextconfig

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const nextConfig = {
   output: 'export',
   // The basePath is essential for GitHub Pages since the site is in a subdirectory.
   basePath: '/portfolio-site',
-  // The assetPrefix is also needed for the same reason.
-  assetPrefix: '/portfolio-site/',
   images: {
     unoptimized: true,
   },
